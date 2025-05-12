3b2b8b82a932f90b856632c7f3243e50e9a845cb
Nguyen_Dang_Shinnraa <nguyenminhdang552k4@gmail.com>
2025-05-12 10:52:47 +0700

    MVC

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,24 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import vn.hoidanit.laptopshop.service.UserService;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return this.userService.handle();
+    }
+
+}
